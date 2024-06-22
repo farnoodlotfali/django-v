3b96747d6585ab962f8158368a01c571bd1fd620
farnoodlotfali <farnoodlotfali@gmail.com>
2024-06-22 16:29:41 +0330

    up 0

--- panel/urls.py
+++ panel/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
+app_name = "Panel"
+
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("teleposts/", views.get_user_posts_view, name="get_user_posts"),
